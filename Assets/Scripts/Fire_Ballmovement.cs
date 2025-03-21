1560cec013bf2123bfd849d3c1076387dee7aa38
Apoorva <apoorvas745sharma@gmail.com>
2025-03-21 10:34:18 +0530

    some minor changes hai

---
+++ Assets/Scripts/Fire_Ballmovement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Fire_Ballmovement : MonoBehaviour
+{
+   
+    public Transform player;
+    public float speed = 3f;
+    public float detectionRange = 5f;
+    private Animator enemyAnimator;
+    private SpriteRenderer enemySpriteRenderer;
+    public AudioSource horrorAudio;
+
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        enemySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        EnemyRun();
+    }
+
+    void EnemyRun()
+    {
+        if (player == null) return;
+
+        Vector2 directionToPlayer = (player.position - transform.position).normalized;
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRange)
+        {
+            transform.position += (Vector3)directionToPlayer * speed * Time.deltaTime;
+           
+            // enemyAnimator.SetBool("Idle", false);
+            
+            // Play horror audio only if it's not already playing
+            if (!horrorAudio.isPlaying)
+            {
+                horrorAudio.Play();
+            }
+
+            // Flip enemy sprite based on direction
+            enemySpriteRenderer.flipX = directionToPlayer.x < 0;
+        }
+        else
+        {
+            
+            // enemyAnimator.SetBool("Idle", true);
+            
+            // Pause the horror audio instead of stopping it immediately
+            if (horrorAudio.isPlaying)
+            {
+                horrorAudio.Pause();
+            }
+        }
+    }
+}
+
